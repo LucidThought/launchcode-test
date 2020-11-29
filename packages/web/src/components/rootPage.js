38267f9c6de9d404fe156a9bbc6681c9666930f8
Andrew Lata <alata.it@gmail.com>
2020-11-29 14:23:49 -0700

    add web

---
+++ packages/web/src/components/rootPage.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./rootPage.scss";
+
+const RootPage = () => {
+  return <Container></Container>;
+};
+
+export default RootPage;
